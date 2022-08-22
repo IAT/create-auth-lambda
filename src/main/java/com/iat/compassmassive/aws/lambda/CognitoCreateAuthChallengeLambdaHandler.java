d341b24532a1b9725b6785f68c4698201f3b60ee
pradeep <pradeep.ghimire@genesesolution.com>
2022-08-22 17:46:10 +0545

    refactored

--- src/main/java/com/iat/compassmassive/aws/lambda/CognitoCreateAuthChallengeLambdaHandler.java
+++ src/main/java/com/iat/compassmassive/aws/lambda/CognitoCreateAuthChallengeLambdaHandler.java
@@ -17,10 +17,7 @@ public class CognitoCreateAuthChallengeLambdaHandler implements RequestHandler<M
     public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
         System.out.println("CREATE AUTH LAMBDA TRIGGERED");
         LambdaLogger log = context.getLogger();
-        System.out.println("Initial event to be returned from Lambda for create auth is : " + event.get("request"));
-        System.out.println("Initial event to be returned from Lambda for create auth is : " + event);
 
-        log.log("Initial event to be returned from Lambda is : " + event);
         Map<String, Object> request = (Map<String, Object>) event.get("request");
         Map<String, Object> response = (Map<String, Object>) event.get("response");
         List<Object> session = (List<Object>) request.get("session");
@@ -29,62 +26,24 @@ public class CognitoCreateAuthChallengeLambdaHandler implements RequestHandler<M
 
         int sessionLength = mapper.convertValue(session, ArrayList.class).size();
         String code = null;
-        System.out.println("Before checking session length:");
-        log.log("Before checking session length:");
         if (sessionLength == 0) {
-//            code = (String) request.get("code");
             String ct = String.valueOf(new Date().getTime());
 
             int l = ct.length();
             code = ct.substring(l - 4);
-            // send email with authentication code
-            try {
-                log.log("Sending mail:" + code);
-//                String authenticationCode = AES.decrypt(code);
-                System.out.println("decryped code:" + code);
-                sendMagicLinkEmail(userAttributes.get("email").toString(), code);
-            } catch (Exception e) {
-                throw new IllegalArgumentException(e.getMessage());
-            }
+
         } else {
             Map<String, Object> previousChallenge = (Map<String, Object>) session.get(sessionLength - 1);
             code = String.valueOf(previousChallenge.get("challengeMetadata").toString().matches("CODE-" + "(\\d*)"));
         }
         Map<String, String> privateChallengeParameters = Collections.singletonMap("ANSWER", code);
-        Map<String, String> publicChallengeParameters = Collections.singletonMap("email", userAttributes.get("email").toString());
+        Map<String, String> publicChallengeParameters = Collections.singletonMap("code", code);
         response.put("privateChallengeParameters", privateChallengeParameters);
         response.put("publicChallengeParameters", publicChallengeParameters);
         response.put("challengeMetadata", "CODE-" + code);
-        System.out.println("response:"+event);
         return event;
     }
 
 
-    private boolean sendMagicLinkEmail(String email, String verificationCode) {
 
-        OkHttpClient client = new OkHttpClient();
-        MediaType mediaType = MediaType.parse("application/json");
-        EmailSender sender = new EmailSender();
-        sender.setEmail(email);
-        sender.setVerificationCode(verificationCode);
-        RequestBody body = null;
-        try {
-            body = RequestBody.create(mediaType, mapper.writeValueAsString(sender));
-            Request clientRequest = new Request.Builder()
-                    .url(POINTS_TEST_PROXY + "/user/send-link")
-                    .method("POST", body)
-                    .addHeader("Content-Type", "application/json")
-                    .build();
-            System.out.println("Client request:" + clientRequest);
-            Response response = client.newCall(clientRequest).execute();
-            System.out.println("Response code:" + response.code());
-            if (response.code() == 200) {
-                return true;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return false;
-    }
 }
